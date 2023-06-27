7166773434c09cb10ad31f8214968b31744891de
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-27 10:29:25 +0300

    Inıtıal controller functions

--- app/controllers/auth_controller.go
+++ app/controllers/auth_controller.go
@@ -1,1 +1,15 @@
 package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func Signup(c *fiber.Ctx) error {
+	return nil
+}
+
+func Login(c *fiber.Ctx) error {
+	return nil
+}
+
+func Logout(c *fiber.Ctx) error {
+	return nil
+}
--- app/controllers/user.controller.go
+++ app/controllers/user.controller.go
@@ -1,1 +1,8 @@
 package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func GetUser(c *fiber.Ctx) error {
+
+	return nil
+}
