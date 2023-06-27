72199230b1617ca6f681d93c0fd107f91e0e0d0f
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-27 14:01:27 +0300

    Add database package with database.go file

--- database/database.go
+++ database/database.go
@@ -1,1 +1,42 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/yusuftalhaklc/go-fiber-authentication/app/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Db *mongo.Database
+
+func Connect() {
+	connectionString := config.Config("DB_URI")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
+	if err != nil {
+		fmt.Print(err)
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Print(err)
+		log.Fatal(err)
+	}
+
+	Db = client.Database(config.Config("DB_NAME"))
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Print(err)
+		log.Fatal(err)
+	}
+
+	log.Println("MongoDB Connection established!")
+}
