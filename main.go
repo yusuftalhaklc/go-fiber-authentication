5753afc57ae6bf1b97fc76bcab2a6ab0819f1d91
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-27 14:07:05 +0300

    fixed bugs for main.go

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -13,11 +15,11 @@ func main() {
 	app.Use(logger.New())
 	app.Use(cors.New())
 
-	app.Use(func(c *fiber.Ctx) error {
-		return c.SendStatus(404)
-	})
-
 	router.SetupRoutes(app)
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(http.StatusNotFound).JSON(fiber.Map{"status": "error", "message": "not found endpoint"})
+	})
 
 	app.Listen(":8080")
 }
