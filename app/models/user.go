92d701e228ba16600d5c3861f28fbf2a93d1f2c2
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-27 14:02:15 +0300

    Add model implementation

--- app/models/user.go
+++ app/models/user.go
@@ -1,1 +1,28 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	User_id       string             `json:"user_id"`
+	First_name    *string            `json:"first_name" validate:"required,min=2,max=100"`
+	Last_name     *string            `json:"last_name" validate:"required,min=2,max=100"`
+	Password      *string            `json:"password" validate:"required,min=6"`
+	Email         *string            `json:"email" validate:"email,required"`
+	Phone         *string            `json:"phone" validate:"required"`
+	Avatar        *string            `json:"avatar"`
+	Token         *string            `json:"token"`
+	Created_at    time.Time          `json:"created_at"`
+	Last_login_at time.Time          `json:"last_login_at"`
+	Logout_at     time.Time          `json:"logout_at"`
+	Deleted_at    time.Time          `json:"deleted_at"`
+}
+
+type LoginResponse struct {
+	Email *string `json:"email"`
+	Token *string `json:"token"`
+}
