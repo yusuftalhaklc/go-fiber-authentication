679e81ecba3a1ff81fb16f42d0fa050abb48ff58
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-27 10:27:29 +0300

    Add main application entry point

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main(){
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/yusuftalhaklc/go-fiber-authentication/app/router"
+)
 
+func main() {
+	app := fiber.New()
+
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+
+	router.SetupRoutes(app)
+
+	app.Listen(":8080")
 }
