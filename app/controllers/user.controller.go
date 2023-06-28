0e5431ce0d69c0daaa0a7cf875be5c16e23ac852
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-28 22:28:17 +0300

    Add function for GET user endpoint in app/controllers directory

--- app/controllers/user.controller.go
+++ app/controllers/user.controller.go
@@ -1,8 +1,22 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/yusuftalhaklc/go-fiber-authentication/app/models"
+	"github.com/yusuftalhaklc/go-fiber-authentication/app/repositories"
+)
 
 func GetUser(c *fiber.Ctx) error {
+	authToken := c.Get("Authorization")
+	var foundUser *models.GetResponse
+	userRepository := repositories.NewUserRepository()
 
-	return nil
+	foundUser, err := userRepository.GetUser(&authToken)
+	if err != nil {
+		return c.Status(http.StatusForbidden).JSON(fiber.Map{"status": "error", "message": err.Error()})
+	}
+
+	return c.Status(200).JSON(fiber.Map{"status": "success", "message": "Successfully found", "data": foundUser})
 }
