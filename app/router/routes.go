b6ab8e89f4805226f6685252ff39243b93256451
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-07-05 10:53:16 +0300

    Implement middlewares

--- app/router/routes.go
+++ app/router/routes.go
@@ -2,17 +2,21 @@ package router
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/yusuftalhaklc/go-fiber-authentication/app/controllers"
+	"github.com/yusuftalhaklc/go-fiber-authentication/app/handlers"
+	middlewares "github.com/yusuftalhaklc/go-fiber-authentication/app/middlewares"
 )
 
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	v1 := api.Group("/user")
 
-	v1.Post("/signup", controllers.Signup)
-	v1.Post("/login", controllers.Login)
-	v1.Put("/update", controllers.Update)
-	v1.Get("/", controllers.GetUser)
-	v1.Post("/logout", controllers.Logout)
-	v1.Post("/delete", controllers.Delete)
+	middleware := middlewares.AuthMiddleware()
+
+	v1.Post("/signup", handlers.Signup)
+	v1.Post("/login", handlers.Login)
+
+	v1.Put("/update", middleware, handlers.Update)
+	v1.Get("/", middleware, handlers.GetUser)
+	v1.Post("/logout", middleware, handlers.Logout)
+	v1.Post("/delete", middleware, handlers.Delete)
 }
