2360778463fb669612b2576e0954b020d6472e6f
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-07-05 10:51:26 +0300

    Add 'AuthMiddleware' function  to  middleware folder

---
+++ app/middlewares/authmiddleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/yusuftalhaklc/go-fiber-authentication/app/utils"
+)
+
+func AuthMiddleware() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		tokenString := c.Cookies("access_token")
+		_, err := utils.VerifyToken(tokenString)
+		if err != nil {
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"status": "error", "message": err.Error()})
+		}
+
+		return c.Next()
+	}
+}
